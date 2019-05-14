8d6ddfe34353667519918676c97cc69785cd31f8
jerrychen44 <jerrychen040@gmail.com>
2019-05-14 16:25:03 +0800

    build a list of blockchain and union/intersection operation for linklist

---
+++ 02_data_structures/project/problem_5/problem5_blockchain.py
@@ -0,0 +1,109 @@
+'''
+Blockchain
+A Blockchain is a sequential chain of records, similar to a linked list.
+Each block contains some information and how it is connected related to the other blocks in the chain.
+Each block contains a cryptographic hash of the previous block, a timestamp, and transaction data.
+For our blockchain we will be using a SHA-256 hash, the Greenwich Mean Time when the block was created,
+and text strings as the data.
+
+Use your knowledge of linked lists and hashing to create a blockchain implementation.
+'''
+
+
+'''
+We can break the blockchain down into three main parts.
+
+First is the information hash:
+We do this for the information we want to store in the block chain such as transaction time,
+data, and information like the previous chain.
+'''
+import hashlib
+import time
+
+
+'''
+The next main component is the block on the blockchain:
+Below is an example of attributes you could find in a Block class.
+'''
+class Block:
+
+    def __init__(self, timestamp, data, previous_hash):
+      self.timestamp = timestamp
+      self.data = data
+      self.previous_hash = previous_hash
+      self.hash = self.calc_hash(data)
+      self.next = None
+
+
+    def calc_hash(self,data):
+          sha = hashlib.sha256()
+
+          #hash_str = "We are going to encode this string of data!".encode('utf-8')
+          hash_str = data.encode('utf-8')
+
+          sha.update(hash_str)
+
+          return sha.hexdigest()
+
+class BlockChiain:
+    def __init__(self):
+        self.head = None
+        self.curnode = None
+        self.elements  = 0
+
+    def insert(self,data):
+
+        if data == None or len(data)==0:
+            print('No input data')
+            return
+
+        timestamp = time.asctime()
+        #print(timestamp)
+
+        #for the first block in this chain
+        if self.head == None:
+            self.head = Block(timestamp,data,'0')
+            self.curnode = self.head
+            self.elements+=1
+            return
+        else:#we already have one block exist
+            pre_hash= self.head.hash
+            #print(pre_hash)
+            newblock = Block(timestamp,data,pre_hash)
+            self.curnode.next = newblock
+            self.curnode = newblock
+            self.elements+=1
+
+    def print_all_chain(self):
+
+        if self.head == None:
+            return
+        tmp= self.head
+        rst = ''
+        while tmp!=None:
+            rst = rst + '[ '+ tmp.timestamp + ',' + tmp.data +',' + tmp.previous_hash +','+ tmp.hash+']'
+            tmp = tmp.next
+        print(rst)
+
+
+'''
+Finally you need to link all of this together in a block chain,
+which you will be doing by implementing it in a linked list.
+All of this will help you build up to a simple but full blockchain implementation!
+'''
+
+blackchain = BlockChiain()
+blackchain.insert('test')
+time.sleep(1)#for different timestamp
+blackchain.print_all_chain()
+blackchain.insert('test2')
+blackchain.print_all_chain()
+time.sleep(2)
+blackchain.insert('apple')
+blackchain.print_all_chain()
+print(blackchain.elements)
+
+
+
+
+#
---
+++ 02_data_structures/project/problem_6/problem6_Union_and_Intersection_of_Two_Linked_Lists.py
@@ -0,0 +1,318 @@
+'''
+Union and Intersection of Two Linked Lists
+Your task for this problem is to fill out the union and intersection functions.
+The union of two sets A and B is the set of elements which are in A, in B, or in both A and B.
+The intersection of two sets A and B, denoted by A ∩ B, is the set of all objects that are members of both the sets A and B.
+
+You will take in two linked lists and return a linked list that is composed of either the union or intersection, respectively.
+Once you have completed the problem you will create your own test cases and perform your own run time analysis on the code.
+
+We have provided a code template below, you are not required to use it:
+'''
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __repr__(self):
+        return str(self.value)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __str__(self):
+        if self.head == None:
+            return 'None of Node in this list'
+        cur_head = self.head
+        out_string = "    "
+        while cur_head:# value/address
+            out_string += str(cur_head.value)+ '/' + hex(id(cur_head))[-4:-1] + " -> "
+            cur_head = cur_head.next
+        return out_string
+
+
+    def append(self, value):
+
+        if self.head is None:
+            self.head = Node(value)
+            return
+
+        node = self.head
+        while node.next:
+            node = node.next
+
+        node.next = Node(value)
+
+    def size(self):
+        size = 0
+        node = self.head
+        while node:
+            size += 1
+            node = node.next
+
+        return size
+
+def union(llist_1, llist_2):
+
+    if llist_1.head == None and llist_2.head != None:
+        llist_1.head = llist_2.head
+        return llist_1
+    elif llist_1.head == None and llist_2.head == None:
+        return 'None of nodes could be union'
+
+    # Your Solution Here
+    appeared_map = {}
+
+    tmpl1 = llist_1.head
+    while tmpl1!=None:
+        appeared_map[tmpl1.value] = 1
+        if tmpl1.next == None:
+            break
+        tmpl1 = tmpl1.next
+
+
+    #now tmpl1 is the tail of llist_1
+    #print(tmpl1)
+    curpointer = tmpl1
+
+    #concatenate to the tail of list 1
+    tmpl2 = llist_2.head
+    while tmpl2 !=None:
+        if tmpl2.value not in appeared_map:
+            appeared_map[tmpl2.value] = 1
+            curpointer.next = tmpl2
+            curpointer = tmpl2
+            tmpl2 = tmpl2.next
+            curpointer.next = None
+        else:
+            tmpl2 = tmpl2.next
+
+
+    #print(appeared_map)
+    return llist_1
+
+
+
+def intersection(llist_1, llist_2):
+    # Your Solution Here
+    appeared_map = {}
+    tmpl1 = llist_1.head
+    while tmpl1!=None:
+        appeared_map[tmpl1.value] = 1
+        if tmpl1.next == None:
+            break
+        tmpl1 = tmpl1.next
+
+    #now tmpl1 is the tail of llist_1
+    #print(tmpl1)
+    curpointer = None
+    llist_1.head = None#we reuse the list 1
+
+
+    #reassign list1.head when we found the intersection item in list2
+    #we rebuild link at the same time when we looping the list2
+    tmpl2 = llist_2.head
+    while tmpl2 !=None:
+
+        #find intersection node
+        if tmpl2.value in appeared_map and appeared_map[tmpl2.value] ==1:
+            appeared_map[tmpl2.value] += 1
+            #first intersection node, we assign the head
+            if llist_1.head == None:
+                llist_1.head = tmpl2
+                tmpl2 = tmpl2.next
+                llist_1.head.next = None
+                curpointer = llist_1.head
+            else:
+                #we linked the next pointer to intersection nodes
+                curpointer.next = tmpl2
+                curpointer = tmpl2
+                tmpl2 = tmpl2.next
+                curpointer.next = None
+        else:
+        #not an intersection node, keep looping list2
+            tmpl2 = tmpl2.next
+
+
+    #print(appeared_map)
+    return llist_1
+
+
+
+
+#####################
+# testing section
+#####################
+
+# Test case 1
+print('test case 1')
+
+linked_list_1 = LinkedList()
+linked_list_2 = LinkedList()
+
+element_1 = [3,2,4,35,6,65,6,4,3,21]
+element_2 = [6,32,4,9,6,1,11,21,1]
+
+for i in element_1:
+    linked_list_1.append(i)
+
+for i in element_2:
+    linked_list_2.append(i)
+
+#print(linked_list_1)
+#print(linked_list_2)
+print('union')
+print (union(linked_list_1,linked_list_2))
+
+################
+# because I reused the list1, so the list1 has been changed as ans which doesn't need extra space.
+# if you need test intersection, please redefined another list1b again
+################
+
+
+linked_list_1b = LinkedList()
+linked_list_2b = LinkedList()
+
+for i in element_1:
+    linked_list_1b.append(i)
+
+for i in element_2:
+    linked_list_2b.append(i)
+
+print('intersection')
+print (intersection(linked_list_1b,linked_list_2b))
+
+
+
+
+# Test case 2
+print('test case 2')
+linked_list_3 = LinkedList()
+linked_list_4 = LinkedList()
+
+element_1 = [3,2,4,35,6,65,6,4,3,23]
+element_2 = [1,7,8,9,11,21,1]
+
+for i in element_1:
+    linked_list_3.append(i)
+
+for i in element_2:
+    linked_list_4.append(i)
+print('union')
+print (union(linked_list_3,linked_list_4))
+
+
+
+
+linked_list_3b = LinkedList()
+linked_list_4b = LinkedList()
+
+
+for i in element_1:
+    linked_list_3b.append(i)
+
+for i in element_2:
+    linked_list_4b.append(i)
+print('intersection')
+print (intersection(linked_list_3b,linked_list_4b))
+
+
+
+
+# Test case 3
+print('test case 3')
+linked_list_5 = LinkedList()
+linked_list_6 = LinkedList()
+
+element_1 = [3,2,4,35,6,65,6,4,3,23]
+element_2 = []
+
+for i in element_1:
+    linked_list_5.append(i)
+
+for i in element_2:
+    linked_list_6.append(i)
+print('union')
+print (union(linked_list_5,linked_list_6))
+
+
+
+
+linked_list_5b = LinkedList()
+linked_list_6b = LinkedList()
+
+
+for i in element_1:
+    linked_list_5b.append(i)
+
+for i in element_2:
+    linked_list_6b.append(i)
+print('intersection')
+print (intersection(linked_list_5b,linked_list_6b))
+
+
+
+# Test case 4
+print('test case 4')
+linked_list_7 = LinkedList()
+linked_list_8 = LinkedList()
+
+element_1 = []
+element_2 = [1,7,8,9,11,21,1]
+
+for i in element_1:
+    linked_list_7.append(i)
+
+for i in element_2:
+    linked_list_8.append(i)
+print('union')
+print (union(linked_list_7,linked_list_8))
+
+
+
+
+linked_list_7b = LinkedList()
+linked_list_8b = LinkedList()
+
+
+for i in element_1:
+    linked_list_7b.append(i)
+
+for i in element_2:
+    linked_list_8b.append(i)
+print('intersection')
+print (intersection(linked_list_7b,linked_list_8b))
+
+
+# Test case 5
+print('test case 5')
+linked_list_9 = LinkedList()
+linked_list_10 = LinkedList()
+
+element_1 = []
+element_2 = []
+
+for i in element_1:
+    linked_list_9.append(i)
+
+for i in element_2:
+    linked_list_10.append(i)
+print('union')
+print (union(linked_list_9,linked_list_10))
+
+
+
+
+linked_list_9b = LinkedList()
+linked_list_10b = LinkedList()
+
+
+for i in element_1:
+    linked_list_9b.append(i)
+
+for i in element_2:
+    linked_list_10b.append(i)
+print('intersection')
+print (intersection(linked_list_9b,linked_list_10b))
