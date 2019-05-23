d2a0c346d4cdcf79a52a160972b022092ac0712d
jerrychen44 <jerrychen040@gmail.com>
2019-05-23 19:47:50 +0800

    add two digit with carry remain

--- 02_data_structures/l1_array_list/11_Detecting_Loops_slow_fast_pointer.py
+++ 02_data_structures/l1_array_list/11_Detecting_Loops_slow_fast_pointer.py
@@ -2,11 +2,11 @@
 # coding: utf-8
 
 # # Detecting Loops in Linked Lists
-# 
+#
 # In this notebook, you'll implement a function that detects if a loop exists in a linked list. The way we'll do this is by having two pointers, called "runners", moving through the list at different rates. Typically we have a "slow" runner which moves at one node per step and a "fast" runner that moves at two nodes per step.
-# 
+#
 # If a loop exists in the list, the fast runner will eventually move behind the slow runner as it moves to the beginning of the loop. Eventually it will catch up to the slow runner and both runners will be pointing to the same node at the same time. If this happens then you know there is a loop in the linked list. Below is an example where we have a slow runner (the green arrow) and a fast runner (the red arrow).
-# 
+#
 # <center><img src='assets/two_runners_circular.png' width=300px></center>
 
 # In[8]:
@@ -16,24 +16,24 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-        
+
 class LinkedList:
     def __init__(self, init_list=None):
         self.head = None
         if init_list:
             for value in init_list:
                 self.append(value)
-        
+
     def append(self, value):
         if self.head is None:
             self.head = Node(value)
             return
-        
+
         # Move to the tail (the last node)
         node = self.head
         while node.next:
             node = node.next
-        
+
         node.next = Node(value)
         return
 
@@ -47,8 +47,8 @@ list_with_loop = LinkedList([2, -1, 3, 0, 5])
 loop_start = list_with_loop.head.next
 
 node = list_with_loop.head
-while node.next: 
-    node = node.next   
+while node.next:
+    node = node.next
 node.next = loop_start
 
 
@@ -66,16 +66,16 @@ def iscircular(linked_list):
     Returns:
        bool: Return True if the linked list is circular, return False otherwise
     """
-    
+
     if linked_list.head == None:
         return False
-    
+
     slow = linked_list.head
     fast = slow
-    
+
     while fast!=None and fast.next!=None:
         if slow.next == slow:
-            return Treu
+            return True
         slow = slow.next
         fast = fast.next.next
         if slow == fast:
@@ -113,24 +113,24 @@ def iscircular(linked_list):
 
     if linked_list.head is None:
         return False
-    
+
     slow = linked_list.head
     fast = linked_list.head
-    
+
     while fast and fast.next:
         # if a node refers to itself, that's a little loop
         if slow.next == slow:
             return True
-        
+
         # slow pointer moves one node
         slow = slow.next
         # fast pointer moves two nodes
         fast = fast.next.next
-        
+
         if slow == fast:
             return True
-    
-    # If we get to a node where fast doesn't have a next node or doesn't exist itself, 
+
+    # If we get to a node where fast doesn't have a next node or doesn't exist itself,
     # the list has an end and isn't circular
     return False
 
@@ -142,7 +142,3 @@ print ("Pass" if  (iscircular(LinkedList([1])) == False) else "Fail")
 
 
 # In[ ]:
-
-
-
-
